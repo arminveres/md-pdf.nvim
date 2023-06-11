8e4e9b8b89b3f12f1a8390195af8344f94a45d54
Armin Veres <armin.veres@hotmail.com>
2023-06-11 21:40:12 +0200

    feat: print stdout/stderr to user messages

--- lua/md-pdf.lua
+++ lua/md-pdf.lua
@@ -1,6 +1,8 @@
 local uv = vim.loop
 
 local M = {}
+local results = {}
+
 
 function M.setup(config)
   if config == nil then
@@ -17,6 +19,30 @@ function M.convert_md_to_pdf()
   -- local stdin = uv.new_pipe()
   local stdout = uv.new_pipe()
   local stderr = uv.new_pipe()
+
+  local function onread(err, data)
+    if err then
+      -- TODO handle err
+      print('ERROR: ', err)
+    end
+    if data then
+      local vals = vim.split(data, "\n")
+      for _, d in pairs(vals) do
+        if d ~= "" then
+          table.insert(results, d)
+        end
+      end
+    end
+  end
+
+  local function print_out()
+    local count = #results
+    for i = 0, count do
+      print(results[i])
+      results[i] = nil -- clear the table for next search
+    end
+  end
+
   handle = uv.spawn(convpath,
     {
       args = { filepath },
@@ -28,29 +54,15 @@ function M.convert_md_to_pdf()
       stdout:close()
       stderr:close()
       handle:close()
+      print_out()
       -- print("exit code", code)
       -- print("exit signal", signal)
     end)
 
   print("process opened", handle)
 
-  uv.read_start(stdout, function(err, data)
-    assert(not err, err)
-    if data then
-      print("stdout chunk", stdout, data)
-    else
-      print("stdout end", stdout)
-    end
-  end)
-
-  uv.read_start(stderr, function(err, data)
-    assert(not err, err)
-    if data then
-      print("stderr chunk", stderr, data)
-    else
-      print("stderr end", stderr)
-    end
-  end)
+  uv.read_start(stdout, onread)
+  uv.read_start(stderr, onread)
 end
 
 return M
