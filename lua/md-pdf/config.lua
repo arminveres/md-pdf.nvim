4d3d3b8a4c832db1b891f333e7d8e4fcddb5f02a
Armin Veres <armin.veres@hotmail.com>
2025-02-03 18:43:31 +0100

    refactor: simplify log api and add warn log method

--- lua/md-pdf/config.lua
+++ lua/md-pdf/config.lua
@@ -1,6 +1,6 @@
 local M = {}
 
----@return string
+---@return string: Command that will open the pdf
 M.default_preview_cmd = function()
     local os_used = vim.uv.os_uname().sysname
     if os_used == "Linux" then
@@ -12,31 +12,36 @@ M.default_preview_cmd = function()
     if os_used == "Windows_NT" then
         return "powershell.exe"
     end
-    require("md-pdf.utils").log_error("Unkown System: " .. os_used)
+    require("md-pdf.utils").log.error("Unkown System: " .. os_used)
 end
 
+---@class md-pdf.config
 local defaults = {
+    ---@type string
     margins = "1.5cm",
-    --- Theme for the pdf document. `tango`, pygments are quite nice for white on white
+    ---@type string Theme for the pdf document. `tango`, pygments are quite nice for white on white
     highlight = "tango",
-    --- Generate a table of contents, on by default
+    ---@type boolean Generate a table of contents, on by default
     toc = true,
-    --- The command to open the pdf with
+    ---@type function The command to open the pdf with
     preview_cmd = M.default_preview_cmd,
-    --- if true, then the markdown file is continuously converted on each write, even if the
-    --- file viewer closed, e.g., firefox is "closed" once the document is opened in it.
+    ---@type boolean if true, then the markdown file is continuously converted on each write,
+    ---even if the file viewer closed, e.g., firefox is "closed" once the document is opened in it.
     ignore_viewer_state = false,
-    --- Specify font, `nil` uses the default font of the theme
+    ---@type table | nil
+    -- Specify font, `nil` uses the default font of the theme
     fonts = nil,
-    --- Custom options passed to `pandoc` CLI call
+    ---@type any Custom options passed to `pandoc` CLI call
     pandoc_user_args = nil,
-    --- Path to output. Needs to be always relative, e.g.: "./", "../", "./out" or simply "out", but
-    --- not absolute e.g.: "/"!
+    ---@type string Path to output. Needs to be always relative, e.g.: "./", "../", "./out" or
+    ---simply "out", but not absolute e.g.: "/"!
     output_path = "",
 }
 
+---@class md-pdf.config
 M.options = {}
 
+---@param options md-pdf.config
 function M.setup(options)
     M.options = vim.tbl_deep_extend("force", defaults, options or {})
 end
